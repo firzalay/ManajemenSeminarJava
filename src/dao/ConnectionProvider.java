21bb5b69ff09bb074510f4204a00394f4b295e8d
firzalay <firza7004@gmail.com>
2025-05-30 23:01:26 +0700

    fix: fix warning about jdbc version and ssl

--- src/dao/ConnectionProvider.java
+++ src/dao/ConnectionProvider.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class ConnectionProvider {
     public static Connection getCon() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_manajemen_seminar_java",
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_manajemen_seminar_java?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
                     "root", "password");
             return con;
         } catch (Exception e) {
